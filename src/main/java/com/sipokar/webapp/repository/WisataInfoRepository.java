73d0954b9ca929e938cf065d6da9d6cf843121b3
Lintang <aydlintang02@gmail.com>
2026-06-23 20:39:04 +0700

    Updated keuangan UMKM

--- src/main/java/com/sipokar/webapp/repository/WisataInfoRepository.java
+++ src/main/java/com/sipokar/webapp/repository/WisataInfoRepository.java
@@ -1,13 +1,10 @@
 package com.sipokar.webapp.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sipokar.webapp.model.User;
+import com.sipokar.webapp.model.WisataInfo;
 
 @Repository
-public interface WisataInfoRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface WisataInfoRepository extends JpaRepository<WisataInfo, Long> {
 }
